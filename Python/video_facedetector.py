e712c66c18cb57894fceb208ca88ee6670727165
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-01-17 15:57:37 -0200

    Corrected code for displaying the frames.

--- Python/video_facedetector.py
+++ Python/video_facedetector.py
@@ -2,7 +2,7 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-faceCascade = cv2.CascadeClassifier(cascPath)
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default_GPU.xml")
 
 def detect(image):
     faces = faceCascade.detectMultiScale(
@@ -13,17 +13,22 @@ def detect(image):
         flags = cv2.cv.CV_HAAR_SCALE_IMAGE
     )
 
+    return faces
+
 while(True):
     rect, image = cap.read()
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    faces, image = detect(gray)
+    faces = detect(gray)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     cv2.imshow("Faces found" ,image)
-    cv2.waitKey(0)
 
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
 cv2.destroyAllWindows()
