197e91ead4d860259818b1a14565bc5fd10aa493
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-01-19 12:59:15 -0200

    Corrected code to load images.

--- Python/grayScale.py
+++ Python/grayScale.py
@@ -3,6 +3,6 @@ import cv2
 i = 0
 
 for i in range(0):
-    image = cv2.imread(str(i) + "jpg", cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread(str(i) + ".jpg", cv2.IMREAD_GRAYSCALE)
     
     cv2.imwrite(str(i) + ".jpg", image)
