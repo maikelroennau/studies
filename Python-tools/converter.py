5428be8a746c40384055458eede889edd5ac889f
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-02-03 21:57:25 -0200

    Updated 'converter.py' script to be more generic.

--- Python-tools/converter.py
+++ Python-tools/converter.py
@@ -2,5 +2,7 @@ import os
 
 i = 0
 for filename in os.listdir("."):
-    os.system("convert " + str(i) + ".pgm " + str(i) + ".jpg")
-    i += 1
+    if not os.path.splitext(filename)[1] == ".py" and not os.path.splitext(filename)[1] == ".jpg": 
+        os.system("convert " + filename + " " + os.path.splitext(filename)[0] + ".jpg")
+        os.system("rm " + filename)
+        i += 1
