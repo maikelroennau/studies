d72a112d162adb415cee365cb37834963d9f4a21
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-02-25 19:54:55 -0300

    Added tensorflow files.

---
+++ TensorFlow/visualization.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    return outputs
+
+# Make up some real data
+x_data = np.linspace(-1, 1, 300, dtype=np.float32)[:, np.newaxis]
+noise = np.random.normal(0, 0.05, x_data.shape).astype(np.float32)
+y_data = np.square(x_data) - 0.5 + noise
+
+plt.scatter(x_data, y_data)
+plt.show()
