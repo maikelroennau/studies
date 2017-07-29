dbf2e137e8d005759c349edfcc1b2f9e2567afae
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-07-29 17:18:32 -0300

    Corrected scripts to move the images with the hights ponctuation.

--- Python-tools/FER_Database/fer_csv_image_extractor.py
+++ Python-tools/FER_Database/fer_csv_image_extractor.py
@@ -1,24 +1,28 @@
-import csv 
+import os
 import sys
+import csv
 import numpy as np
-from PIL import Image
 import cv2
- 
-filename = open(sys.argv[1], 'rb')
+from PIL import Image
 
+
+filename = open('fer2013.csv', 'rb')
 file = csv.reader(filename)
 
-i = 1
+location = 'FER'
 
+i = 1
 for row in file:
     if row[0] == 'emotion':
         continue
 
     image = np.fromstring(row[1], dtype=int, sep=' ').reshape(48, 48)
 
-    location = row[2] + "/"
+    if not os.path.isdir(os.path.join(location, row[2])):
+        os.makedirs(os.path.join(location, row[2]))
+        print(os.path.join(location, row[2]))
 
-    cv2.imwrite(location + "FER_" + str(i) + ".jpg", image)
+    cv2.imwrite(os.path.join(location, row[2]) + '/FER_' + str(i) + '.jpg', image)
 
     i += 1
 
--- Python-tools/FER_Database/mover.py
+++ Python-tools/FER_Database/mover.py
@@ -2,31 +2,26 @@ import csv
 import sys
 import os
 
-filename = open(sys.argv[1], 'rb')
-
+filename = open('fer2013new.csv', 'rb')
 file = csv.reader(filename)
 
-destination = 'test/'
-
-i = 1
+origin = 'FER'
+destination = 'FER+/'
 emotions = ['neutral', 'happiness', 'surprise', 'sadness', 'anger', 'disgust', 'fear', 'contempt', 'unknown', 'NF']
 
+i = 1
 for row in file:
     if row[0] == 'Usage':
         continue
 
-    if max(row[1:]) in ['10', '9']:
-        from_path = row[0] + "/"
-        destine_path = os.path.join(destination, row[0] + "/", emotions[row[1:].index(max(row[1:]))] + "/")
+    if not os.path.isdir(os.path.join(destination, row[0], emotions[row[1:].index(max(row[1:]))])):
+        os.makedirs(os.path.join(destination, row[0], emotions[row[1:].index(max(row[1:]))]))
 
-        os.rename(from_path + "FER_" + str(i) + ".jpg", destine_path + "FER_" + str(i) + ".jpg")
+    origin_path = os.path.join(origin, row[0], "FER_" + str(i) + ".jpg")
+    destination_path = os.path.join(destination, row[0], emotions[row[1:].index(max(row[1:]))]) + "/FER_" + str(i) + ".jpg"
 
+    os.rename(origin_path, destination_path)
+     
     i += 1
 
 filename.close()
-
-
-import os
-emotions = ['neutral', 'happiness', 'surprise', 'sadness', 'anger', 'disgust', 'fear', 'contempt', 'unknown', 'NF']
-for i in emotions:
-    os.makedirs(i)
