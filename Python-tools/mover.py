d8b00ddea714b9d69d40f3d958470727da4355e3
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-08-06 17:24:54 -0300

    Corrected folder creation.

--- Python-tools/mover.py
+++ Python-tools/mover.py
@@ -8,10 +8,10 @@ for dir in os.listdir('.'):
 
         print('Processing {}'.format(dir))
 
-        for i, image in enumerate(os.listdir(dir)):
-            if not os.path.isdir(os.path.join(destine, dir)):
-                os.makedirs(os.path.join(destine, dir))
+        if not os.path.isdir(os.path.join(destine, dir)):
+            os.makedirs(os.path.join(destine, dir))
 
+        for i, image in enumerate(os.listdir(dir)):
             os.system('mv {} {}'.format(os.path.join(dir, image), os.path.join(destine, dir)))
 
             if i + 1 == images:
