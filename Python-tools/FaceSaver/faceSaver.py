577fd4ac97c1c77138663188719b8d604d685877
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-09-02 18:28:25 -0300

    New version of face saver. Also, adding the classifier.

---
+++ Python-tools/FaceSaver/faceSaver.py
@@ -0,0 +1,25 @@
+import numpy as np
+import os
+import cv2
+
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+for directory in os.listdir("."):
+    if directory[-3:] != ".py" and directory[-4:] != '.xml':
+	print 'Processing {}'.format(directory)
+        images = os.listdir(directory)
+
+        for img in images:
+            image = cv2.imread(os.path.join(directory, img))
+            faces = faceCascade.detectMultiScale(
+                image,
+                scaleFactor=1.5,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags = 0
+            )
+
+            for (x, y, w, h) in faces:
+                face = image[y: y + h, x: x + w]
+                cv2.imwrite(os.path.join(directory, img), face)
+                # cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
