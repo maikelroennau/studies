abe9526cf62fbb9c33a1059ff2c0057f4939b7b0
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2017-11-12 00:49:53 -0200

    Update automatic_opencv_installator.sh

--- OpenCV/automatic_opencv_installator.sh
+++ OpenCV/automatic_opencv_installator.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install build-essential
-sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
-sudo apt-get install libgtkglext1 libgtkglext1-dev
+sudo apt-get upgrade -y
+sudo apt-get install build-essential -y
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev -y
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev -y
+sudo apt-get install libgtkglext1 libgtkglext1-dev -y
 mkdir OpenCV
 cd OpenCV
 git clone https://github.com/opencv/opencv.git
