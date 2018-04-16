f95111cd52751e67608f38c12816743e40886790
Maikel Maciel Rönnau <maikel.ronnau@gmail.com>
2018-04-16 01:55:49 +0000

    Updated train script.

--- Keras/train.py
+++ Keras/train.py
@@ -1,3 +1,6 @@
+import os
+import json
+
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
@@ -5,21 +8,23 @@ from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
 
 
-# dimensions of our images.
-img_width, img_height = 48, 48
+train_data_dir = 'dataset/train'
+validation_data_dir = 'dataset/validation'
 
-train_data_dir = 'CRFK/train'
-validation_data_dir = 'CRFK/validation'
-nb_train_samples = 4943
-nb_validation_samples = 700
-epochs = 20
-batch_size = 35
+img_width, img_height = 128, 128
+
+epochs = 5
+batch_size = 32
+
+model_name = 'first_try.h5'
+model_dir = 'models'
 
 if K.image_data_format() == 'channels_first':
     input_shape = (3, img_width, img_height)
 else:
     input_shape = (img_width, img_height, 3)
 
+# Model
 model = Sequential()
 model.add(Conv2D(32, (3, 3), input_shape=input_shape))
 model.add(Activation('relu'))
@@ -37,23 +42,16 @@ model.add(Flatten())
 model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(7))
+model.add(Dense(2))
 model.add(Activation('softmax'))
 
-model.compile(loss='sparse_categorical_crossentropy',
+model.compile(loss='sparse_categorical_crossentropy', # categorical_crossentropy
               optimizer='adam',
               metrics=['accuracy'])
 
-# this is the augmentation configuration we will use for training
-train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
 
-# this is the augmentation configuration we will use for testing:
-# only rescaling
-test_datagen = ImageDataGenerator(rescale=1. / 255)
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
@@ -61,17 +59,21 @@ train_generator = train_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='binary')
 
+json = json.dumps(train_generator.class_indices)
+f = open(os.path.join(model_dir, '{}_classes.json'.format(os.path.splitext(model_name)[0])), 'w')
+f.write(json)
+f.close()
+
 validation_generator = test_datagen.flow_from_directory(
     validation_data_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
     class_mode='binary')
 
+
 model.fit_generator(
     train_generator,
-    steps_per_epoch=nb_train_samples // batch_size,
     epochs=epochs,
-    validation_data=validation_generator,
-    validation_steps=nb_validation_samples // batch_size)
+    validation_data=validation_generator)
 
-model.save('first_try.h5')
+model.save(os.path.join(model_dir, model_name))
